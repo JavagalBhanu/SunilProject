c6c841e3871eab2151b8839710b3994990c3d4d3
grhav <grhav@DESKTOP-1335TBB>
2021-02-18 22:33:06 +1100

    code is commited as it is new

---
+++ MavenPilotProjet/src/test/java/abcd/Demo.java
@@ -0,0 +1,13 @@
+package abcd;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void testa () {
+		Reporter.log("Test method",true);
+	}
+	
+	
+}
